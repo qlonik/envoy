1a328bcd6f7f5dfa2dbbeb6657ea17a1e33e348a
Nikita Volodin <volodin.n@gmail.com>
2023-12-16 20:37:50 -0500

    feat: merge golang-http with jaeger-tracing

--- examples/jaeger-tracing/verify.sh
+++ examples/jaeger-tracing/verify.sh
@@ -3,9 +3,16 @@
 export NAME=jaeger-tracing
 export PORT_PROXY="${JAEGER_PORT_PROXY:-11010}"
 export PORT_UI="${JAEGER_PORT_UI:-11011}"
+export MANUAL=true
 
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
+
+run_log "Compile the go plugin library"
+"${DOCKER_COMPOSE[@]}" -f docker-compose.compile.yaml up --quiet-pull --remove-orphans go_plugin_compile
+
+run_log "Start all of our containers"
+bring_up_example
 
 wait_for 10 bash -c "responds_with Hello http://localhost:${PORT_PROXY}/trace/1"
 
