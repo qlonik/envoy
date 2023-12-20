a85c25c0a6478fa9b7dc9c06707f53f84cae934e
Nikita Volodin <volodin.n@gmail.com>
2023-12-20 18:11:50 -0500

    feat: merge golang-http with opentelemetry example

--- examples/opentelemetry/verify.sh
+++ examples/opentelemetry/verify.sh
@@ -3,9 +3,18 @@
 export NAME=opentelemetry
 export PORT_PROXY="${OPENTELEMETRY_PORT_PROXY:-12000}"
 export PORT_UI="${OPENTELEMETRY_PORT_UI:-12001}"
+export MANUAL=true
 
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
+
+run_log "Compile the go plugin library"
+"${DOCKER_COMPOSE[@]}" -f docker-compose.compile.yaml up --quiet-pull --remove-orphans go_plugin_compile
+
+run_log "Start all of our containers"
+bring_up_example
+
+sleep 2
 
 run_log "Make a request to service-1"
 responds_with \
@@ -21,3 +30,6 @@ run_log "View the traces in OpenTelemetry UI"
 responds_with \
     "<!DOCTYPE html>" \
     "http://localhost:${PORT_UI}/debug/tracez"
+
+run_log "Service logs"
+"${DOCKER_COMPOSE[@]}" logs envoy-front-proxy opentelemetry
